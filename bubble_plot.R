18db254378078d3e645d2702af265a993fca9986
saulosgil <saulosgil@hotmail.com>
2024-03-08 08:46:51 -0300

    bubble plot - test

---
+++ bubble_plot.R
@@ -0,0 +1,35 @@
+# Libraries
+library(ggplot2)
+library(dplyr)
+library(hrbrthemes)
+library(viridis)
+
+# The dataset is provided in the gapminder library
+data <- gapminder |>
+  filter(year == "2007") |>
+  dplyr::select(-year)
+
+readr::write_csv(data, 'data.csv')
+
+# Most basic bubble plot
+data |>
+  arrange(desc(pop)) |>
+  mutate(country = factor(country, country)) |>
+  ggplot(aes(
+    x = gdpPercap,
+    y = lifeExp,
+    size = pop,
+    fill = continent
+  )) +
+  geom_point(alpha = 0.5,
+             shape = 21,
+             color = "black") +
+  scale_size(range = c(.1, 24), name = "Population (M)") +
+  scale_fill_viridis(discrete = TRUE,
+                     guide = FALSE,
+                     option = "A") +
+  theme_ipsum() +
+  theme(legend.position = "bottom") +
+  ylab("Life Expectancy") +
+  xlab("Gdp per Capita") +
+  theme(legend.position = "none")
